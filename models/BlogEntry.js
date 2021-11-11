d9841b9cb22d875f1e867e554838527cd121b1f3
brooke <brookejones406@gmail.com>
2021-11-10 20:51:16 -0600

    models

---
+++ models/BlogEntry.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class BlogEntry extends Model {}
+
+BlogEntry.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+        },
+        post_content: {
+            type: DataTypes.STRING,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW,
+        },
+    },
+    {
+        sequelize,
+        timestamps: flase,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogentry',
+    }    
+);
+
+module.exports = BlogEntry;
