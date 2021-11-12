4c03553a91b82ea9c52511ed81c431f8ec0e14a8
brooke <brookejones406@gmail.com>
2021-11-11 21:41:38 -0600

    rearranged folders

---
+++ public/js/edit.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const id = document.getElementById('postUpdate').innerHTML
+    const name = document.querySelector('#post_name').value.trim();
+    const post_content = document.getElementById('#post_content').value.trim();
+
+    if (name && post_content) {
+        const response = await fetch(`/api/blogentry/edit/${id}`, {
+            method: 'POST',
+            body: JSON.stringify({ id, name, post_content }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert("Sorry I ain't made yer post.");
+        }
+    }
+};
+
+const delButtonHandler = async (event) => {
+    if (event.target.getAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+const response = await fetch(`/api/blogentry/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        document.location.replace('/posts');
+    } else {
+        alert("Sorry I caint delete yer post.");
+    }
+
+document
+    .querySelector('.new-comment-form')
+    .addEventListener('submit', newCommentHandler);
+
+document 
+    .querySelector('.project-list')
+    .addEventListener('click', delButtonHandler)
+
