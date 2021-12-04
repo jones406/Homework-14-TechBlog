45ec3f69cfa274b9eb73ccace840732b2517fbdb
brooke <brookejones406@gmail.com>
2021-12-03 21:19:24 -0600

    jaws db things

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'specialsecret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
